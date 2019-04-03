f69de5ff3433a0610ad76c3acc499bc50e3bb3a1
treia <marc@planelles.com>
2019-04-03 21:46:01 +0200

    Switched doom back to tommorow night

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -11,7 +11,7 @@
   doom-font (font-spec :family "SF Mono" :size 14)
   doom-unicode-font (font-spec :family "DejaVu Sans" :size 14)
   doom-big-font (font-spec :family "SF Mono" :size 18)
-  doom-theme 'doom-ayu-dark
+  doom-theme 'doom-tomorrow-night
   +modeline-buffer-path-function '+modeline-file-name
   scroll-conservatively 0
   show-trailing-whitespace t
