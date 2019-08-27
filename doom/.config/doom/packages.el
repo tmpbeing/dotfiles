a31be733ded85045160aac8e1b14d1afe7835d4d
treia <marc@planelles.com>
2019-08-27 22:56:16 +0200

    Doom: Updated package declaration for straight

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -4,7 +4,10 @@
 ;; Org
 (package! ob-http) ;; curl in org-mode literate programming
 (package! org-pomodoro) ;; pomodoro clock directly inside org-mode
-(package! ox-jekyll :recipe (:fetcher github :repo "tmpbeing/ox-jekyll-subtree")) ;; Org-file to jekyll blog
+(package! ox-jekyll :recipe (:host github :repo "tmpbeing/ox-jekyll-subtree")) ;; Org-file to jekyll blog
+
+;; Themes
+(package! vibrant-ink-theme :recipe (:host github :repo "arkhan/vibrant-ink-theme" :files ("vibrant-ink-theme.el")))
 
 ;; Misc
 ; (package! systemd)
