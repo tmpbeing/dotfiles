c9582f72b51d3dd02cabb3c41c3a9836d76f674c
treia <marc@planelles.com>
2019-09-11 13:13:56 +0200

    Doom: org-pomodoro now using manual break
    Meaning they are not automatically over once the timer expires but
    instead go into overtime until manually stopped

--- doom/.config/doom/+org.el
+++ doom/.config/doom/+org.el
@@ -6,7 +6,9 @@
 (def-package! org-pomodoro
   :after org
   :config
-  (setq org-pomodoro-finished-sound +org-gong-sound
+  (setq org-pomodoro-manual-break t
+        org-pomodoro-finished-sound +org-gong-sound
+        org-pomodoro-overtime-sound +org-gong-sound
         org-pomodoro-short-break-sound +org-bell-sound
         org-pomodoro-long-break-sound +org-bell-sound)
   ; Alert is used by org-pomodoro to send notifitications
@@ -25,46 +27,29 @@
         org-ellipsis " ▼ "
         org-refile-target-verify-function '+org/custom-verify-target)
 
-  (setq org-capture-templates
-        '(("t" "Personal todo" entry
-           (file+headline +org-capture-todo-file "Inbox")
-           "* [ ] %?\n%i\n%a" :prepend t :kill-buffer t)
-          ("n" "Personal notes" entry
-           (file+headline +org-capture-notes-file "Inbox")
-           "* %u %?\n%i\n%a" :prepend t :kill-buffer t)
+  (appendq! org-capture-templates
+            '("b" "Bookmark" entry
+              (file+headline +org-capture-bookmark-file "To read/Classify")
+              "* [[%x][%?]] %^g" :kill-buffer t)
+            '("z" "Pomodoro" entry ; Used for polybar integration
+              (file+headline +org-capture-todo-file "Inbox")
+              "* [ ] %?\n%i\n%a" :prepend t :kill-buffer t))
 
-          ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
-          ;; {todo,notes,changelog}.org file is found in a parent directory.
-          ("p" "Templates for projects")
-          ("pt" "Project todo" entry  ; {project-root}/todo.org
-           (file+headline +org-capture-project-todo-file "Inbox")
-           "* TODO %?\n%i\n%a" :prepend t :kill-buffer t)
-          ("pn" "Project notes" entry  ; {project-root}/notes.org
-           (file+headline +org-capture-project-notes-file "Inbox")
-           "* TODO %?\n%i\n%a" :prepend t :kill-buffer t)
-          ("pc" "Project changelog" entry  ; {project-root}/changelog.org
-           (file+headline +org-capture-project-notes-file "Unreleased")
-           "* TODO %?\n%i\n%a" :prepend t :kill-buffer t)
+(set-popup-rule! "^\\Org Agenda"
+  :size 15
+  :quit t
+  :select t
+  :parameters
+  '((transient)))
 
-          ("b" "Bookmark" entry
-           (file+headline +org-capture-bookmark-file "To read/Classify")
-           "* [[%x][%?]] %^g" :kill-buffer t)))
+(add-hook 'org-clock-in-hook #'save-buffer)
+(add-hook 'org-clock-out-hook #'save-buffer)
 
-  (set-popup-rule! "^\\Org Agenda"
-    :size 15
-    :quit t
-    :select t
-    :parameters
-    '((transient)))
-
-  (add-hook 'org-clock-in-hook #'save-buffer)
-  (add-hook 'org-clock-out-hook #'save-buffer)
-
-  (map! :map org-mode-map
-        :localleader
-        "p" #'org-pomodoro
-        (:prefix ("c" . "clock")
-          "r" #'org-clock-report)))
+(map! :map org-mode-map
+      :localleader
+      "p" #'org-pomodoro
+      (:prefix ("c" . "clock")
+        "r" #'org-clock-report)))
 
 (map! :leader
       (:prefix "n"
