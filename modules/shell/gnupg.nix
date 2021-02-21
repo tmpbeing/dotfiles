99b926d7ad6516da11de3736e901202187786217
tmpbeing <marc@planelles.dev>
2021-02-21 22:00:18 +0100

    Fix gnupg module
    Signed-off-by: tmpbeing <marc@planelles.dev>

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -3,10 +3,10 @@
 with lib;
 with lib.my;
 let cfg = config.modules.shell.gnupg;
-in
-{
-  options.modules.shell.gnupg = {
+in {
+  options.modules.shell.gnupg = with types; {
     enable = mkBoolOpt false;
+    cacheTTL = mkOpt int 3600;
   };
 
   config = mkIf cfg.enable {
@@ -15,5 +15,12 @@ in
     programs.gnupg.agent.enable = true;
 
     user.packages = with pkgs; [ tomb ];
+
+    home.configFile."gnupg/gpg-agent.conf" = {
+      text = ''
+        default-cache-ttl ${toString cfg.cacheTTL}
+        pinentry-program ${pkgs.pinentry.gtk2}/bin/pinentry
+      '';
+    };
   };
 }
