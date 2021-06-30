008ae023ca365c10d126a10eee354ccf518a1a64
tmpbeing <marc@extrality.ai>
2021-06-30 23:39:13 +0200

    [WIP] Updated polybar config
    Still some things to fix

--- polybar/.config/polybar/spotify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-player_status=$(playerctl status 2> /dev/null)
-
-if [ "$player_status" = "Playing" ]; then
-	echo "%{F#958090} $(playerctl metadata --format "{{ title }} - {{ artist }}")"
-elif [ "$player_status" = "Paused" ]; then
-	echo "%{F#7DAEA9} $(playerctl metadata --format "{{ title }} - {{ artist }}")"
-fi
