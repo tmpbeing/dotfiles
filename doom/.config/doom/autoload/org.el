697139948c01aacd3f0d4a7db1b655a955bd9191
tmpbeing <marc@extrality.ai>
2021-07-20 18:56:02 +0200

    Started updating my org-pomodoro system

--- doom/.config/doom/autoload/org.el
+++ doom/.config/doom/autoload/org.el
@@ -29,3 +29,84 @@
   (dolist (buffer (+workspace-buffer-list))
     (with-current-buffer buffer (when (derived-mode-p 'org-agenda-mode)
                                   (org-agenda-redo t)))))
+
+;;;###autoload
+(defcustom +org-polybar/clock-format-style 'simple
+  "Controls the content of +org/format-clock-time. Should be one of simple, long, full")
+
+;;;###autoload
+(defvar +org-polybar/last-known-state 'inactive
+  "Interval variable that stores the last known state recorded on message update.")
+
+
+;;; TODO: Make configurable
+;;;###autoload
+(defun +org-polybar/icon-from-state (state)
+  "Build string for icon from the state"
+  (interactive "SState :")
+  (pcase state
+    (':pomodoro "%{F#9b9257}\ue001")
+    (':overtime "%{F#8d7856}\ue003")
+    (':short-break "%{F#738c9c}\ue005")
+    (':long-break "%{F#6998b3}\ue006")
+    ('clock "%{F#798362}")
+    (_ "%{F#acb3b5}\ue007"))
+  )
+
+;;;###autoload
+(defun +org-polybar/format-status-string ()
+  "Get currently clocked time string, with support for org-pomodoro and estimates, for polybar integration"
+  (interactive)
+                                        ; TODO: Don't require that here ? + Not interactive
+  (require 'org-clock)
+  (require 'org-pomodoro)
+  (let* ((state (cond ((org-pomodoro-active-p) org-pomodoro-state)
+                      ((org-clock-is-active) 'clock)
+                      (t 'inactive)))
+         (polybar-icon (+org-polybar/icon-from-state state)))
+    (set '+org-polybar/last-known-state state)
+    (if (or (eq +org-polybar/clock-format-style 'simple) (eq state 'inactive)) polybar-icon
+      (let ((clock-timer-string (cond ((eq +org-polybar/clock-format-style 'simple) "")
+                                      ((org-pomodoro-active-p) (org-pomodoro-format-seconds))
+                                      ((org-clock-is-active)
+                                       (let ((clocked-time (org-duration-from-minutes (org-clock-get-clocked-time))))
+                                        ; Might need to org-duration-to-minutes + org-duration-from-minutes the content of org-clock-effort
+                                         (if org-clock-effort (let (effort-time (org-duration-from-minutes org-clock-effort))
+                                                                (format "%s/%s" clocked-time effort-time))
+                                           clocked-time)))
+                                      (t ""))))
+        (if (or (eq +org-polybar/clock-format-style 'long)
+                (member state '(:short-break :long-break)))
+            (format "%s %s" polybar-icon clock-timer-string)
+          (format "%s %s %s" polybar-icon clock-timer-string org-clock-current-task))))))
+
+;;;###autoload
+(defun +org-polybar/clock-format-style-toggle ()
+  "Toggle between the possible states for +org-polybar/clock-format-style"
+  (interactive)
+  (set '+org-polybar/clock-format-style (pcase +org-polybar/clock-format-style
+                                          ('simple 'long)
+                                          ('long 'full)
+                                          (_ 'simple))))
+
+;;;###autoload
+(defun +org-polybar/toggle-pomodoro-or-clock ()
+  "Start a new pomodoro if inactive, ends current pomodoro/clock otherwise"
+  (if (eq +org-polybar/last-known-state 'clock) (org-clock-out) (org-pomodoro)))
+
+;; (defun +org-polybar/end-or-restart ()
+;;   "End the currently active pomodoro or org-clock if any.
+;; If inactive, will instead restart the last known pomodoro/clock as a new pomodoro.
+;; ;; TODO: Restart clock as org-clock, not pomodoro ?
+
+;; Note that it uses the last known state to determine which action to take."
+;;   (pcase +org/polybar/last-known-state
+;;     ('clock org-clock-out)
+;;          (':pomodoro nil)
+;;          (':overtime org-pomodoro-finished)
+;;          (_ +org-pomodoro/restart-last-pomodoro))
+;;                                         ; clock -> kill ; Can be handled separately
+;;                                         ; pomodoro -> nothing
+;;                                         ; overtime -> finish
+;;                                         ; else -> restart
+;;        )
--- polybar/.config/polybar/modules/org-pomodoro.sh
+++ polybar/.config/polybar/modules/org-pomodoro.sh
@@ -5,7 +5,7 @@
 # The file where to currently used output style is saved
 OUTPUT_STYLE_FILE="/tmp/pomodoro-polybar-output-style"
 # How to call your emacsclient
-EMACSCLIENT="emacsclient"
+EMACSCLIENT="emacsclient --socket=org-emacs"
 # If you use doom-emacs, this should be true, false otherwise
 DOOM_EMACS=true
 # The key used by your todo org-capture template
@@ -15,119 +15,15 @@ MY_POMODORO_TEMPLATE=z
 
 ### Script starts here
 
-# If the file doesn't exist, default to SIMPLE output style
-if test -e "$OUTPUT_STYLE_FILE"
-then
-    OUTPUT_STYLE=$(cat "$OUTPUT_STYLE_FILE")
-else
-    OUTPUT_STYLE="SIMPLE"
-fi
-
-set_state() {
-    STATE=$($EMACSCLIENT --eval '(if (org-pomodoro-active-p) org-pomodoro-state -1)' 2>&1)
-}
-
-# Colors have to be hardcoded. See : https://github.com/polybar/polybar/wiki/Formatting#format-tags
-set_pomodoro_icon() {
-    case "$STATE" in
-        ":pomodoro")
-            ICON="%{F#B77A76}\ue002";;
-        ":overtime")
-            ICON="%{F#B77A76}\ue003";;
-        ":short-break")
-            ICON="%{F#958090}\ue005";;
-        ":long-break")
-            ICON="%{F#7DAEA9}\ue006";;
-        *)
-            ICON="\ue007";;
-    esac
-}
-
-set_remaining_time() {
-    TIME_REMAINING=$($EMACSCLIENT --eval '(org-pomodoro-format-seconds)')
-    TIME_REMAINING="${TIME_REMAINING%\"}" # Bash trick to remove leading and trailing quotes
-    TIME_REMAINING="${TIME_REMAINING#\"}"
-}
-
-set_task_at_hand() {
-    TASK_AT_HAND=$($EMACSCLIENT --eval '(org-no-properties org-clock-current-task)')
-    TASK_AT_HAND="${TASK_AT_HAND%\"}" # Bash trick to remove leading and trailing quotes
-    TASK_AT_HAND="${TASK_AT_HAND#\"}"
-}
-
-# Print corresponding pomodoro icon and exit
-print_pomodoro_state_simple() {
-    set_state
-    set_pomodoro_icon
-    echo -e "$ICON"
-    exit
-}
-
-# Print pomodoro icon and time remaining
-print_pomodoro_state_long() {
-    set_state
-    set_pomodoro_icon
-    if [ $STATE = ":pomodoro" -o $STATE = ":short-break" -o $STATE = ":long-break" ]; then
-        set_remaining_time
-        echo -e "$ICON $TIME_REMAINING"
-    elif [ $STATE = ":overtime" ]; then
-        set_remaining_time
-        echo -e "$ICON +$TIME_REMAINING"
-    else # Org-pomodoro not running, nothing else to print
-        echo -e "$ICON"
-    fi
-    exit
-}
-
-print_pomodoro_state_full() {
-    set_state
-    set_pomodoro_icon
-    if [ $STATE = ":pomodoro" ]; then
-        set_remaining_time
-        set_task_at_hand
-        echo -e "$ICON $TIME_REMAINING $TASK_AT_HAND"
-    elif [ $STATE = ":overtime" ]; then
-        set_remaining_time
-        set_task_at_hand
-        echo -e "$ICON +$TIME_REMAINING $TASK_AT_HAND"
-    elif [ $STATE = ":short-break" -o $STATE = ":long-break" ]; then #No current task to print
-        set_remaining_time
-        echo -e "$ICON $TIME_REMAINING"
-    else # Org-pomodoro not running, nothing else to print
-        echo -e "$ICON"
-    fi
-    exit
-}
-
-update() {
-    case "$1" in
-        "SIMPLE")
-            print_pomodoro_state_simple;;
-        "LONG")
-            print_pomodoro_state_long;;
-        *)
-            print_pomodoro_state_full;;
-    esac
-}
-
 if [ "$#" == "0" ]; then
-    update $OUTPUT_STYLE
+    STATUS=$($EMACSCLIENT --eval '(+org-polybar/format-status-string)')
+    STATUS="${STATUS%\"}" # Bash trick to remove leading and trailing quotes
+    STATUS="${STATUS#\"}"
+    echo -e $STATUS
 fi
 
-# Toggle env variable to know what kind of output to show
 if [ "$1" == "toggle" ]; then
-    case "$OUTPUT_STYLE" in
-        "SIMPLE")
-            NEW_OUTPUT_STYLE="LONG";;
-        "LONG")
-            NEW_OUTPUT_STYLE="FULL";;
-        *)
-            NEW_OUTPUT_STYLE="SIMPLE";;
-    esac
-    echo $NEW_OUTPUT_STYLE > /tmp/pomodoro-polybar-output-style
-    # For now echoing the output on click does not update the bar so this would be pointless
-    # Keep an eye on : https://github.com/polybar/polybar/issues/720
-    # update $NEW_OUTPUT_STYLE
+    $EMACSCLIENT --eval '(+org-polybar/clock-format-style-toggle)'
     exit
 fi
 
@@ -152,15 +48,7 @@ if [ "$1" == "new" ]; then
 fi
 
 if [ "$1" == "end-or-restart" ]; then
-    set_state
-    if [ $STATE = ":pomodoro" ]; then # A pomodoro is already running, do nothing
-        exit
-    elif [ $STATE = ":overtime" ]; then # End current pomodoro if in overtime
-        $EMACSCLIENT --eval '(org-pomodoro-finished)'
-    else # Call pomodoro with prefix argument to make it restart last clocked-in pomodoro
-        $EMACSCLIENT --eval '(+org-pomodoro/restart-last-pomodoro)'
-    fi
-    # update $OUTPUT_STYLE
+    $EMACSCLIENT --eval '(+org-polybar/toggle-pomodoro-or-clock)'
     exit
 fi
 
