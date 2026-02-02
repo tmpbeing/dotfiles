aa97ec410fb12a7280fa37ae4e1a74b7cd6c61fc
Marc Planelles <marc@extrality.ai>
2026-02-02 22:50:27 +0100

    chore(emacs): cleanup packages.el

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -1,36 +1,41 @@
 ;; -*- no-byte-compile: t; -*-
-;;; ~/.doom.d/packages.el
+;;; $DOOMDIR/packages.el
 
-;; Org
-(package! ob-http) ;; curl in org-mode literate programming
-(package! ox-gfm)
-(package! ox-jekyll :recipe (:host github :repo "tmpbeing/ox-jekyll-subtree")) ;; Org-file to jekyll blog
-(package! ob-mermaid) ;; draw diagrams in org-mode with mermaid-js
-(package! org-chef) ;; Import recipes from common sites into org-mode
-(package! doct) ;; Better capture template declaration
-(package! org-present)
 
 ;; Languages
 (package! systemd)
-
+(package! kdl-mode)
+(package! mermaid-mode :recipe (:host github :repo "shen390s/mermaid-mode")) ;; edit mermaid diagrams
 (package! elixir-ts-extras
   :recipe (:host github :repo "wkirschbaum/elixir-ts-extras")
   )
 ;; elixir-ts-extras provides better testing functions for elixir
 (package! exunit :disable t)
 
-;; Misc
-(package! mermaid-mode :recipe (:host github :repo "shen390s/mermaid-mode")) ;; edit mermaid diagrams
-(package! nov) ;; epub reader
+;; Org
+(package! ob-http) ;; curl in org-mode literate programming
+(package! ox-gfm)
+(package! ox-jekyll :recipe (:host github :repo "tmpbeing/ox-jekyll-subtree")) ;; Org-file to jekyll blog
+(package! org-chef) ;; Import recipes from common sites into org-mode
+(package! doct) ;; Better capture template declaration
+(package! ob-mermaid) ;; draw diagrams in org-mode with mermaid-js
 (package! org-media-note :recipe (:host github :repo "yuchen-lea/org-media-note"))
 
-;; ;; Local dev
-;(package! spelunk
-;  :recipe (:local-repo "spelunk" :no-byte-compile t))
+;; Misc
+(package! nov) ;; epub reader
+(package! night-owl-theme)
+(package! doom-moonfly-theme
+  :recipe (:host github
+           :repo "stackmystack/doom-moonfly-theme"))
+(package! kubernetes)
+(package! kubernetes-evil)
+(package! jj-mode :recipe (:host github :repo "bolivier/jj-mode.el"))
+(package! mason)
+(package! difftastic)
+(package! magit-todos)
 
 
-;; Package configuration
-(use-package! mermaid-mode
-  :defer true)
-(use-package! nov
-  :defer true)
+;; Local dev
+
+;; (package! spelunk
+;; recipe (:local-repo "spelunk" :no-byte-compile t))
